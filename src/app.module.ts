4f03a7fcdfa785942210c183799fa80b66acf9ed
adzamkomladev <adzamkomla.dev@gmail.com>
2020-05-16 20:24:40 +0000

    Remove app controller, controller spec, and service files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.dev.env', load: [databaseConfig] })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.dev.env',
+      load: [databaseConfig],
+    }),
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}

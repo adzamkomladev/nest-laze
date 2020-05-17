2034f7c93f6816ec56fc65b5d05d6f3c1237a416
adzamkomladev <adzamkomla.dev@gmail.com>
2020-05-17 23:05:31 +0000

    Refactor (misc.)

--- src/projects/dtos/update-project.dto.ts
+++ src/projects/dtos/update-project.dto.ts
@@ -3,9 +3,9 @@ import { IsOptional, IsString } from 'class-validator';
 export class UpdateProjectDto {
   @IsOptional()
   @IsString()
-  readonly title: string;
+  readonly title?: string;
 
   @IsOptional()
   @IsString()
-  readonly details: string;
+  readonly details?: string;
 }

25445e3bb57eff0283f7833b290d654f57a72159
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-04 11:59:11 +0000

    Add web socket chat functionality: Part 1

--- src/chats/gateways/chats.gateway.ts
+++ src/chats/gateways/chats.gateway.ts
@@ -1,9 +1,40 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private readonly logger: Logger;
+
+  @WebSocketServer() server: Server;
+
+  constructor() {
+    this.logger = new Logger(ChatsGateway.name);
+  }
+
+  @SubscribeMessage('messageToServer')
+  handleMessage(client: Socket, payload: string): void {
+    this.server.emit('messageToClient', payload);
+  }
+
+  afterInit(server: Server): void {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket): void {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]): void {
+    this.logger.log(`Client connected: ${client.id}`);
   }
 }

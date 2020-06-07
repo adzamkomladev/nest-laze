1b3ffe43767c53e277d228effffa510ac5cabaac
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-07 20:32:50 +0000

    add default port for heroku

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT, 10) || 80,
+  port: parseInt(process.env.APP_PORT, 10) || 5000,
   version: process.env.APP_VERSION || 'v1',
 }));

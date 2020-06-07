88bf9238f5a882e2922f4ae41be3029d664c2e05
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-07 18:16:21 +0000

    Update configs for remote db

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   port: parseInt(process.env.APP_PORT, 10) ?? 80,
-  version: process.env.APP_VERSION,
+  version: process.env.APP_VERSION ?? 'v1',
 }));
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,12 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  type: process.env.DB_TYPE ?? 'mysql',
+  host: process.env.DB_HOST ?? 'db4free.net',
+  port: parseInt(process.env.DB_PORT, 10) ?? 3306,
+  database: process.env.DB_DATABASE ?? 'nest_laze',
+  username: process.env.DB_USERNAME ?? 'nest_laze',
+  password: process.env.DB_PASSWORD ?? 'nest_laze',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 }));

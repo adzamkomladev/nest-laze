0954f162367d1ea877eae1077ee5e9d088cbf6c5
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-07 20:15:52 +0000

    add postgres variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,16 +2,17 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: process.env.DB_TYPE || 'postgres',
-  url:
-    process.env.DB_URL ||
-    'postgres://ohkjavvqvuzzef:7202822579fcfc7fe600a169946e2f710ab6b2bdef388e7ecee0dd912739a6ea@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d45r48do5ti2qv',
-  // host: process.env.DB_HOST || 'ec2-3-222-30-53.compute-1.amazonaws.com',
-  // port: parseInt(process.env.DB_PORT, 10) || 5432,
-  // database: process.env.DB_DATABASE || 'd45r48do5ti2qv',
-  // username: process.env.DB_USERNAME || 'ohkjavvqvuzzef',
-  // password:
-  //   process.env.DB_PASSWORD ||
-  //   '7202822579fcfc7fe600a169946e2f710ab6b2bdef388e7ecee0dd912739a6ea',
+  // url:
+  //   process.env.DB_URL ||
+  //   'postgres://ohkjavvqvuzzef:7202822579fcfc7fe600a169946e2f710ab6b2bdef388e7ecee0dd912739a6ea@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d45r48do5ti2qv',
+  host: process.env.DB_HOST || 'ec2-3-222-30-53.compute-1.amazonaws.com',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  database: process.env.DB_DATABASE || 'd45r48do5ti2qv',
+  username: process.env.DB_USERNAME || 'ohkjavvqvuzzef',
+  password:
+    process.env.DB_PASSWORD ||
+    '7202822579fcfc7fe600a169946e2f710ab6b2bdef388e7ecee0dd912739a6ea',
+  ssl: { rejectUnauthorized: false },
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
 }));

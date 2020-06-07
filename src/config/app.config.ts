c5ddfde02d037bd5927f553df89997b0fea5312b
adzamkomladev <adzamkomla.dev@gmail.com>
2020-06-07 16:21:54 +0000

    Add heroku confis

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: parseInt(process.env.APP_PORT, 10),
+  port: parseInt(process.env.APP_PORT, 10) ?? 80,
   version: process.env.APP_VERSION,
 }));
